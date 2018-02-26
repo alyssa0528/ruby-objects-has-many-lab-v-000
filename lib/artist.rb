762338d3664620d6dfba2d7c5d8d6b3be2ecf2c8
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-26 13:20:03 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require 'pry'
+class Artist 
+  attr_accessor :name
+  
+  @@song_count = 0 
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end 
+  
+  def add_song(song)
+    @songs << song 
+    @@song_count += 1 
+    song.artist = self 
+  end 
+  
+  def songs 
+    @songs 
+  end 
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self #self here refers to the instance of Artist 
+  end 
+  
+  def self.song_count
+    @@song_count 
+  end 
+end 
