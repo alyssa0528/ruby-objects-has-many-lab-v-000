28fc64763434d27919f34be18d5b08d71dc7fe3d
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-26 13:48:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,9 +6,9 @@ class Song
   end 
   
   def artist_name
-    if self.artist.name == nil 
-      nil 
-    else self.artist.name 
+    if self.artist.name 
+      self.artist.name 
+    else nil
     end 
   end 
 end 
